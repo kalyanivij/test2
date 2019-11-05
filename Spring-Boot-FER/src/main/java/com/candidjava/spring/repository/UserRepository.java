7e94ddbbba9dcf3712b0a497278c38a3ce1fcf82
dell <dell@DESKTOP-T43RPRT>
2019-11-05 16:51:54 +0530

    one more line i added

--- Spring-Boot-FER/src/main/java/com/candidjava/spring/repository/UserRepository.java
+++ Spring-Boot-FER/src/main/java/com/candidjava/spring/repository/UserRepository.java
@@ -15,4 +15,6 @@ public interface UserRepository extends CrudRepository<User, Long>{
 	
 	List<User> findByNameAndCountry(String name, String country);
 
+	List<User> findByNameOrCountry(String name, String country);
+
 }
