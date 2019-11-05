312ddd98a55cff3125f5682a28da211f3cb2c1e7
dell <dell@DESKTOP-T43RPRT>
2019-11-05 16:37:43 +0530

    In userRepository few lines i added

--- Spring-Boot-FER/src/main/java/com/candidjava/spring/repository/UserRepository.java
+++ Spring-Boot-FER/src/main/java/com/candidjava/spring/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.candidjava.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.candidjava.spring.bean.User;
-public interface UserRepository extends CrudRepository<User, Long>
-{
+public interface UserRepository extends CrudRepository<User, Long>{
 	
+	List<User> findByName (String name);
+	
+	List<User> findByCountry (String country);
+
+	List<User> findById (long id);
 
 }
